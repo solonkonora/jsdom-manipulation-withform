fa6fd7f29ff8f22e5d5e541dafe382ebe8e6eef1
Nkwada Nora Solonko <nkwadanora@mail.com>
2023-12-09 01:15:21 -0800

    write the filterstudent function

--- dommanipulation.js
+++ dommanipulation.js
@@ -1,6 +1,16 @@
 const studentResult = document.getElementById('student-list')
 const stuForms = document.getElementById('filter-form')
-const inputName = document.getElementById('Name')
+const inputName = document.getElementById('name')
 const inputAge = document.getElementById('age')
 
-// adding event listener for submission to the form
+
+
+function filterStudents (name, age) {
+  const students = [
+    { name: 'John', age: 20 },
+    { name: 'Emily', age: 22 },
+    { name: 'Michael', age: 19 },
+    { name: 'Sophia', age: 21 },
+    { name: 'Daniel', age: 20 },
+  ];
+}
