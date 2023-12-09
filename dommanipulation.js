f6fb5fe4fde9da3ce8c148ac51df469eac531fed
Nkwada Nora Solonko <nkwadanora@mail.com>
2023-12-09 00:47:15 -0800

    defining js variables

---
+++ dommanipulation.js
@@ -0,0 +1,6 @@
+const studentResult = document.getElementById('student-list')
+const stuForms = document.getElementById('filter-form')
+const inputName = document.getElementById('Name')
+const inputAge = document.getElementById('age')
+
+// adding event listener for submission to the form
